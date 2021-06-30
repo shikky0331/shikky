21a1fb188d872f0a084497f1d9b49211acea28c7
shikky0331 <shikky0331>
2021-07-01 08:19:55 +0900

    refactor: move page folder to containers folder

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -3,7 +3,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import Home from 'pages/home/index';
+import Home from 'containers/pages/home/index';
 import { VFC } from 'react';
 
 const Containers: VFC = () => (
