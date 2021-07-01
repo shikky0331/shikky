2fcd17eed4c725736b36f84f8e2fb0a65e827529
shikky0331 <shikky0331>
2021-07-01 20:53:39 +0900

    chore: remove trailing paths that are not needed

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -3,7 +3,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import Pages from 'containers/pages/index';
+import Pages from 'containers/pages';
 import { VFC } from 'react';
 
 const Containers: VFC = () => (
