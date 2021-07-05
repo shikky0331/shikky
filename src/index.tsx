8f2d52fb896220cbb7189d8261fa10193adf6d95
shikky0331 <shikky0331>
2021-07-06 08:44:07 +0900

    feat: add global.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import './index.scss';
+import 'styles/global.scss';
 import { StrictMode } from 'react';
 import Containers from 'containers';
 import reportWebVitals from './reportWebVitals';
