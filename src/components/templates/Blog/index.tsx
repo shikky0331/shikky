5e3a6c31fada6e7af62aedce99afebb78aec462f
shikky0331 <shikky0331>
2021-07-07 09:01:58 +0900

    chore: because it becomes global scope

--- src/components/templates/Blog/index.tsx
+++ src/components/templates/Blog/index.tsx
@@ -6,29 +6,29 @@ const Blog: VFC = () => (
   <main className={styles.main}>
     <article>
       <article className={styles.post}>
-        <h3><Link to="https://shikky0331.github.io/shikky/">fifth blog post</Link></h3>
+        <h3 className={styles.h3}><Link to="https://shikky0331.github.io/shikky/">fiveth blog post</Link></h3>
         <small>February 13, 2021</small>
-        <p>This is my 5th blog post</p>
+        <p className={styles.p}>This is my 5th blog post</p>
       </article>
       <article className={styles.post}>
-        <h3><Link to="https://shikky0331.github.io/shikky/">forth blog post</Link></h3>
+        <h3 className={styles.h3}><Link to="https://shikky0331.github.io/shikky/">forth blog post</Link></h3>
         <small>March 23, 2021</small>
-        <p>This is my 4th blog post</p>
+        <p className={styles.p}>This is my 4th blog post</p>
       </article>
       <article className={styles.post}>
-        <h3><Link to="https://shikky0331.github.io/shikky/">third blog post</Link></h3>
+        <h3 className={styles.h3}><Link to="https://shikky0331.github.io/shikky/">third blog post</Link></h3>
         <small>April 4, 2021</small>
-        <p>This is my 3th blog post</p>
+        <p className={styles.p}>This is my 3th blog post</p>
       </article>
       <article className={styles.post}>
-        <h3><Link to="https://shikky0331.github.io/shikky/">second blog post</Link></h3>
+        <h3 className={styles.h3}><Link to="https://shikky0331.github.io/shikky/">second blog post</Link></h3>
         <small>May 30, 2021</small>
-        <p>This is my 2th blog post</p>
+        <p className={styles.p}>This is my 2th blog post</p>
       </article>
       <article className={styles.post}>
-        <h3><Link to="https://shikky0331.github.io/shikky/">first blog post</Link></h3>
+        <h3 className={styles.h3}><Link to="https://shikky0331.github.io/shikky/">first blog post</Link></h3>
         <small>July 30, 2021</small>
-        <p>This is my 1th blog post</p>
+        <p className={styles.p}>This is my 1th blog post</p>
       </article>
     </article>
   </main>
