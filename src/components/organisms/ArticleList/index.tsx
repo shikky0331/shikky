14c3aadb7c2c6696e9ced5b62e66bee12da6f531
shikky0331 <shikky0331>
2021-07-20 09:07:43 +0900

    refactor: extract ArticleList component as organisms of Atomic Design

---
+++ src/components/organisms/ArticleList/index.tsx
@@ -0,0 +1,20 @@
+import Article, { ArticleProps } from 'components/molecules/Article';
+import { VFC } from 'react';
+
+interface ArticleListProps {
+  articles: ArticleProps[]
+}
+
+const ArticleList: VFC<ArticleListProps> = ({
+  articles,
+}) => (
+  <>
+    {articles.map(({
+      url, title, date, introduction,
+    }) => (
+      <Article url={url} title={title} date={date} introduction={introduction} />
+    ))}
+  </>
+);
+
+export default ArticleList;
