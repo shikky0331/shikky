58c8397a2aa6e75bc1ae7bd357efedc12d4e99a4
shikky0331 <shikky0331>
2021-07-26 09:06:08 +0900

    fix: change url in article
    Use url as key of list so need to change url to different value

--- src/containers/pages/index.tsx
+++ src/containers/pages/index.tsx
@@ -4,19 +4,19 @@ import Blog from 'components/templates/Blog';
 const Pages: VFC = () => {
   const articles = [
     {
-      url: '/', title: 'How Are Function Components Defferent from Classes?', date: 'February 13, 2021', introduction: 'This is my 5th blog post',
+      url: '/posts/5', title: 'How Are Function Components Defferent from Classes?', date: 'February 13, 2021', introduction: 'This is my 5th blog post',
     },
     {
-      url: '/', title: 'forth blog post', date: 'March 23, 2021', introduction: 'This is my 4th blog post',
+      url: '/posts/4', title: 'forth blog post', date: 'March 23, 2021', introduction: 'This is my 4th blog post',
     },
     {
-      url: '/', title: 'third blog post', date: 'April 4, 2021', introduction: 'This is my 3th blog post',
+      url: '/posts/3', title: 'third blog post', date: 'April 4, 2021', introduction: 'This is my 3th blog post',
     },
     {
-      url: '/', title: 'second blog post', date: 'May 30, 2021', introduction: 'This is my 2th blog post',
+      url: '/posts/2', title: 'second blog post', date: 'May 30, 2021', introduction: 'This is my 2th blog post',
     },
     {
-      url: '/', title: 'first blog post', date: 'July 21, 2021', introduction: 'This is my 1th blog post',
+      url: '/posts/1', title: 'first blog post', date: 'July 21, 2021', introduction: 'This is my 1th blog post',
     },
   ];
 
