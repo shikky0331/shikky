efd45eaada9735837a0d620aca7d8b292fe56b42
shikky0331 <shikky0331>
2021-07-30 08:46:59 +0900

    feat: add Footer component to storybook

---
+++ src/components/organisms/Footer/index.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react';
+
+import { Story, Meta } from '@storybook/react';
+
+import Footer from './index';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+} as Meta;
+
+const Template: Story<ComponentProps<typeof Footer>> = () => <Footer />;
+
+export const Default = Template.bind({});
+Default.args = {
+};
