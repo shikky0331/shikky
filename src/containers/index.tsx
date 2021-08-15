6968c8c69ad710a9880e6770ce6da9ea2722882a
shikky0331 <shikky0331>
2021-08-15 11:18:49 +0900

    refactor: rename pages/blog/index to pages/blog/date/index

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -4,7 +4,7 @@ import {
   Route,
 } from 'react-router-dom';
 import Pages from 'containers/pages';
-import Blog from 'containers/pages/blog';
+import Blog from 'containers/pages/blog/date';
 import { VFC } from 'react';
 
 const Containers: VFC = () => (
