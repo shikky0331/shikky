42e012abd28ab9539577b63252a4bdf08524060f
shikky0331 <shikky0331>
2021-08-18 09:34:17 +0900

    feat: add react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import 'styles/global.scss';
 import { StrictMode } from 'react';
 import Containers from 'containers';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <StrictMode>
-    <Containers />
-  </StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(<StrictMode><Containers /></StrictMode>, rootElement);
+} else {
+  render(<StrictMode><Containers /></StrictMode>, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
