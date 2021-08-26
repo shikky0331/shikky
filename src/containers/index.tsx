b32efc4866b831c2fdd0fb77e3b3b28577eba4c8
shikky0331 <shikky0331>
2021-08-26 19:33:44 +0900

    feat: add react-helmet to containers/index
    To define a basic seo.

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -6,18 +6,32 @@ import {
 import Pages from 'containers/pages';
 import Blog from 'containers/pages/blog/date';
 import { VFC } from 'react';
+import { Helmet } from 'react-helmet';
 
 const Containers: VFC = () => (
-  <Router basename={process.env.PUBLIC_URL}>
-    <Switch>
-      <Route path="/blog/:date">
-        <Blog />
-      </Route>
-      <Route path="/">
-        <Pages />
-      </Route>
-    </Switch>
-  </Router>
+  <>
+    <Helmet
+      defaultTitle="Shikky"
+      titleTemplate="%s | Shikky"
+    >
+      <title>Shikky</title>
+      <link rel="canonical" href="https://shikky0331.github.io/shikky" />
+      <meta charSet="utf-8" />
+      <meta property="og:site_name" content="Shikky" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@shikky0331" />
+    </Helmet>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/blog/:date">
+          <Blog />
+        </Route>
+        <Route path="/">
+          <Pages />
+        </Route>
+      </Switch>
+    </Router>
+  </>
 );
 
 export default Containers;
