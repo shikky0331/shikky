51da89d547f9a5b1e68b7c1f50afbf873c95cba3
shikky0331 <shikky0331>
2021-08-28 09:22:34 +0900

    refactor: summarize a SEO concerned code to templates/Seo component
    Becase there is only one place to see SEO related code.

--- src/components/templates/Seo/index.tsx
+++ src/components/templates/Seo/index.tsx
@@ -20,14 +20,22 @@ const Seo: VFC<SeoProps> = ({
   ogDescription,
   ogImage,
 }) => (
-  <Helmet>
+  <Helmet
+    defaultTitle="Shikky"
+    titleTemplate="%s | Shikky"
+  >
     <title>{title}</title>
+    <link rel="canonical" href="https://shikky0331.github.io/shikky" />
+    <meta charSet="utf-8" />
     <meta name="description" content={description} />
     <meta property="og:url" content={url} />
     <meta property="og:type" content={ogType} />
     <meta property="og:title" content={ogTitle} />
     <meta property="og:description" content={ogDescription} />
     <meta property="og:image" content={ogImage} />
+    <meta property="og:site_name" content="Shikky" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@shikky0331" />
   </Helmet>
 );
 
--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -6,21 +6,9 @@ import {
 import Pages from 'containers/pages';
 import Blog from 'containers/pages/blog/date';
 import { VFC } from 'react';
-import { Helmet } from 'react-helmet';
 
 const Containers: VFC = () => (
   <>
-    <Helmet
-      defaultTitle="Shikky"
-      titleTemplate="%s | Shikky"
-    >
-      <title>Shikky</title>
-      <link rel="canonical" href="https://shikky0331.github.io/shikky" />
-      <meta charSet="utf-8" />
-      <meta property="og:site_name" content="Shikky" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@shikky0331" />
-    </Helmet>
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/blog/:date">
